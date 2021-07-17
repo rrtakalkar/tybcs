80580f8f77bccfa5b0f531dd96954dd2f03f4971
rrtakalkar <rutuja.r.takalkar@gmail.com>
2021-07-17 23:04:37 +0530

    pushing latest changes

--- java/Add.java
+++ java/Add.java
@@ -1,26 +1,21 @@
 import java.util.*;
-
 class Adddemo {
     Scanner obj = new Scanner(System.in);
     int a, b;
-
-    void accept() {
+     void accept() {
         System.out.println("Enter two no's");
         a = obj.nextInt();
         b = obj.nextInt();
     }
-
-    void sum() {
+     void sum() {
         int c = a + b;
         System.out.println("Addition is :=> " + c);
 
     }
-
-    int sum_test(int a, int b) {
+     int sum_test(int a, int b) {
         return a + b;
     }
-
-    void substract() {
+     void substract() {
         int G = 0;
         if (a < b) {
             G = b - a;
@@ -29,41 +24,33 @@ class Adddemo {
         }
         System.out.println("Substraction is :=>" + G);
     }
-
-    int substract_test(int a, int b) {
+     int substract_test(int a, int b) {
         return a - b;
     }
-
-    void multi() {
+     void multi() {
         int c = a * b;
         System.out.println("Multiplication is:=>" + c);
     }
-
-    int multi_test(int a, int b) {
+     int multi_test(int a, int b) {
         return a * b;
     }
-
-    void div() {
+     void div() {
         int c = a / b;
         System.out.println("Division is:=>" + c);
     }
-
-    int Div_test(int a, int b) {
+     int Div_test(int a, int b) {
         return a / b;
     }
 }
-
 class Addtest {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         Adddemo t1 = new Adddemo();
         t1.accept();
         t1.sum();
         t1.substract();
         t1.multi();
         t1.div();
-
-        int result = t1.substract_test(10, 20);
-
+      int result = t1.substract_test(10, 20);
         System.out.println("Subsctraction Test result =>" + result);
         int result1 = t1.sum_test(30, 40);
         System.out.println("Addition Test result=>" + result1);
